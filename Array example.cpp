f0f4d57579c77a28c58038f0fe2a4f1a43046370
Meisam Taheri <60437951+Taheri-meisam@users.noreply.github.com>
2020-11-19 15:48:08 +0100

    fixes

--- Array example.cpp
+++ Array example.cpp
@@ -5,14 +5,14 @@
 class ArrayClass
 {
 public:
-    static const int row = 2, col = 5;
-    int status1 = 0, status2 = 0, status3 = 0, status4 = 0, status5 = 0;
+    static constexpr int row = 2, col = 5;
+    int status[5]{ 0 };
     int dice1 = rand() % 6 + 1, dice2 = rand() % 6 + 1, dice3 = rand() % 6 + 1, dice4 = rand() % 6 + 1, dice5 = rand() % 6 + 1;
 
     int diceBoard[row][col]
     {
         {dice1, dice2, dice3, dice4, dice5},
-        {status1, status2, status3, status4, status5}
+        {this->status[0], this->status[1], this->status[2],this->status[3], this->status[4]}
     };
 
 public:
@@ -38,36 +38,30 @@ public:
     void holdChoice()
     {
         std::cout << "Please type the number of the dice you would like to reroll.\n";
-        for (int i = 0; i < col; i++)
-        {
-            switch (_getch())
-            {
-            case '1':
-                 = 1;
-                break;
-            case '2':
-                status2 = 1;
-                break;
-            case '3':
-                status3 = 1;
-                break;
-            case '4':
-                status4 = 1;
-                break;
-            case '5':
-                status5 = 1;
-            }
-            system("cls");
-            printBoard();
-        }
-
         for (int i = 1; i < col; i++)
         {
-            
-            
-
-            
+            switch (i+1)
+            {
+            case 1:
+                diceBoard[1][i] = 1;
+                break;
+            case  2:
+                diceBoard[1][i] = 1;
+                break;
+            case 3:
+                diceBoard[1][i] = 1;
+                break;
+            case 4:
+                diceBoard[1][i] = 1;
+                break;
+            case 5:
+                diceBoard[1][i] = 1;
+                break;
+            }
+         //   system("cls");
+           
         }
+
     }
 };
 
@@ -76,6 +70,7 @@ int main()
     srand(std::time(nullptr));
 
     ArrayClass a;
+    a.holdChoice();
     a.arraySolution();
 }
 
